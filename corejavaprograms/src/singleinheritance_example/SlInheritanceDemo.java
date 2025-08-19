e7624ff143fa39753752ed264e660cfe8210c193
kavya <kavya@LAPTOP-EEGOMD4V>
2025-08-19 14:15:58 +0530

    Inheritance

---
+++ corejavaprograms/src/singleinheritance_example/SlInheritanceDemo.java
@@ -0,0 +1,15 @@
+ package singleinheritance_example;
+
+public class SlInheritanceDemo {
+
+        public static void main(String[] args) {
+
+                Trainer t = new Trainer(001,"Adithiyaa","Puducherry",01,"SOFTWARE DEVELOPER","Chennai");
+                System.out.println(t);
+
+                Person a = new Person(113,"Adithiyaa","Puducherry");
+                System.out.println(a);
+
+        }
+
+}
---
+++ corejavaprograms/src/singleinheritance_example/Trainer.java
@@ -0,0 +1,49 @@
+package singleinheritance_example;
+
+public class Trainer extends Person {
+
+        private int eid;
+        private String designation;
+        private String location;
+
+        //Parameterized Constructor
+        public Trainer(int pid, String name, String city, int eid, String designation, String location) {
+
+                super(pid, name, city);
+                this.eid = eid;
+                this.designation = designation;
+                this.location = location;
+        }
+
+        public int getEid() {
+                return eid;
+        }
+
+        public void setEid(int eid) {
+                this.eid = eid;
+        }
+
+        public String getDesignation() {
+                return designation;
+        }
+
+        public void setDesignation(String designation) {
+                this.designation = designation;
+        }
+
+        public String getLocation() {
+                return location;
+        }
+
+        public void setLocation(String location) {
+                this.location = location;
+        }
+
+        @Override
+        public String toString() {
+                return "trainer [personid= "+ super.getPid()+" ,personName= "+ super.getName()+",personCity="+ super.getCity()+", id=" + eid + ", designation=" + designation + ", location=" + location + "]";
+        }
+
+
+
+}
