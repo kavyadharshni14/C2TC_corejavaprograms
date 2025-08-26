fdb7f00bc3848de4aa4cdde30ebfa28f328b5408
kavya <kavya@LAPTOP-EEGOMD4V>
2025-08-26 11:29:01 +0530

    day nine completed

---
+++ corejavaprograms/src/com/tnsif/daynine/StringSubClass.java
@@ -0,0 +1,7 @@
+package com.tnsif.daynine;
+
+public class StringSubClass {
+    public static void main(String[] args) {
+        System.out.println("You cannot extend the String class because it is final.");
+    }
+}
