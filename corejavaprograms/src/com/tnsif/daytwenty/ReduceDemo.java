a6358e36e36797a5a5cf54004423c33fa48dbe93
kavya <kavya@LAPTOP-EEGOMD4V>
2025-08-29 23:08:57 +0530

    this project is about stream functions

---
+++ corejavaprograms/src/com/tnsif/daytwenty/ReduceDemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.daytwenty;
+
+public class ReduceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ corejavaprograms/src/com/tnsif/daytwenty/StreamBasics.java
@@ -0,0 +1,10 @@
+package com.tnsif.daytwenty;
+
+public class StreamBasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
